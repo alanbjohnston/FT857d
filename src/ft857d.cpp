d300141f52609102a0b3d093b8885f47f09cabf1
Alan Johnston <alan.b.johnston@gmail.com>
2026-06-23 15:27:35 -0400

    Update ft857d.cpp don't get char

--- src/ft857d.cpp
+++ src/ft857d.cpp
@@ -164,8 +164,8 @@ void ft857d::check() {
     uint8_t i = serialDataAvail(serial_fd);
     if (i > 0) {
         printf(" %d ", i);
-        int j = serialGetchar(serial_fd);
-        printf(" %d-%s", j, j);
+//        int j = serialGetchar(serial_fd);
+//        printf(" %d-%s", j, j);
         return;
     }
     if (i < 5) return;

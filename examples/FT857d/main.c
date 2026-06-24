5b1a27dff9c80a4b834d264b5658c127abd884cd
Alan Johnston <alan.b.johnston@gmail.com>
2026-06-24 08:58:20 -0400

    Update main.c printf

--- examples/FT857d/main.c
+++ examples/FT857d/main.c
@@ -171,7 +171,7 @@ uint8_t catGetTXStatus() {
 //    uint8_t r = 1;
     // we fix the TX power to half scale (8)
     r = ptt<<7 + splitActive<<5 + 8;
-    print("Sending %x \n",r);
+    printf("Sending %x \n",r);
 
     return r;
 }
